34ac694ed5a5b92faed2b26ba390a4e45da188b7
Nganjie <nganjienzatsi@gmail.com>
2023-04-03 06:02:14 +0100

    pour la page infoVehicule

--- js/pageinfoVehicule.js
+++ js/pageinfoVehicule.js
@@ -1,6 +1,24 @@
 var section_info = document.querySelector(".info-vehicule");
 console.log("un mode de paie");
+console.log(document.cookie);
+console.log(getCookie("nom"));
+var p_nom =document.querySelector(".p-nom");
+var profil =document.querySelector(".profil");
+var span = profil.querySelector("span")
+console.log(span);
 
+
+
+function getCookie(name) {
+    const cookies = document.cookie.split('; ')
+    const value = cookies
+        .find(c => c.startsWith(name + "="))
+        ?.split('=')[1]
+    if (value === undefined) {
+        return null
+    } 
+    return decodeURIComponent(value)
+}
 var str = window.location.href;
 console.log(str);
 var url = new URL(str);
@@ -32,7 +50,11 @@ console.log(form_api)
        })
        console.log(button_command);
        button_command.addEventListener("click",()=>{
-        alert("les infirmations du vendeur sont disponible dans la rébrique  contacts du vendeurs pour vous aidé à le contacté");
+        if(getCookie("nom"))
+        {
+            alert("votre commande à été notifier au vendeur, il vous contactera d'ici peut. vous disposer ausssi de ses contacts pour faire le prémier pas");
+        }else alert(" veillez vous indentifié avant de passer une commande");
+
        })
        
     })
